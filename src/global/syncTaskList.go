69be2dd0e07f77796d6f81f04719272ab1aeabb1
Lluisaac <isaaclluis@gmail.com>
2023-08-16 09:26:36 +0200

    [SRCH OVRLL] Improved the SyncTaskList

--- src/global/syncTaskList.go
+++ src/global/syncTaskList.go
@@ -6,6 +6,8 @@ import (
 
 /*
 This is a useful task manager that will execute tasks one by one in the same goroutine.
+
+It is safe to try to add another task as part of a task.
 */
 type SyncTaskList struct {
 	mutex sync.Mutex
@@ -23,16 +25,18 @@ func (stl *SyncTaskList) AddTask(task func()) {
 }
 
 func (stl *SyncTaskList) doTasks() {
+	stl.mutex.Lock()
+	defer stl.mutex.Unlock()
+
 	for len(stl.tasks) > 0 {
 		firstTask := stl.getFirstTask()
+		stl.mutex.Unlock()
 		firstTask()
+		stl.mutex.Lock()
 	}
 }
 
 func (stl *SyncTaskList) getFirstTask() func() {
-	stl.mutex.Lock()
-	defer stl.mutex.Unlock()
-
 	current := stl.tasks[0]
 	stl.tasks = stl.tasks[1:]
 
