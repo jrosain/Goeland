35da7070c8b86030d2b7ab230cb275699bd5ae04
Lluisaac <isaaclluis@gmail.com>
2023-08-23 13:28:51 +0200

    [ARI] Upgraded the Form struct to be more generalized

--- src/plugins/arithmetic/arithBuilder.go
+++ src/plugins/arithmetic/arithBuilder.go
@@ -32,10 +32,10 @@ func GetCounterExample(formNetworks []basictypes.FormList) (example CounterExamp
 }
 
 func buildConstraintNetwork(formNetworks []basictypes.FormList) ([]Network, map[string]basictypes.Term) {
-	a := MakeSimpleConstraint(&Variable{"X"}, GreaterEq, &Constant{0})
-	b := MakeSimpleConstraint(&Variable{"X"}, GreaterEq, &Constant{1})
-	c := MakeSimpleConstraint(&Variable{"X"}, GreaterEq, &Constant{-5})
-	d := MakeSimpleConstraint(&Variable{"X"}, LesserEq, &Constant{0})
+	a := MakeSimpleConstraint(NewVariable("X"), GreaterEq, NewConstant(0))
+	b := MakeSimpleConstraint(NewVariable("X"), GreaterEq, NewConstant(1))
+	c := MakeSimpleConstraint(NewVariable("X"), GreaterEq, NewConstant(-5))
+	d := MakeSimpleConstraint(NewVariable("X"), LesserEq, NewConstant(0))
 
 	termMap := make(map[string]basictypes.Term)
 	termMap["X"] = basictypes.MakeEmptyMeta()
--- src/plugins/arithmetic/arithForm.go
+++ src/plugins/arithmetic/arithForm.go
@@ -10,40 +10,183 @@ type Form interface {
 	global.Basic[Form]
 }
 
+type Integer int
+
+func (i Integer) ToString() string {
+	return strconv.Itoa(int(i))
+}
+
+func (i Integer) Equals(other any) bool {
+	if typed, ok := other.(Integer); ok {
+		return i == typed
+	}
+	return false
+}
+
+func (i Integer) Copy() Form {
+	return i
+}
+
+type String string
+
+func (s String) ToString() string {
+	return string(s)
+}
+
+func (s String) Equals(other any) bool {
+	if typed, ok := other.(String); ok {
+		return s == typed
+	}
+	return false
+}
+
+func (s String) Copy() Form {
+	return s
+}
+
+type SimpleForm[T global.Basic[Form]] struct {
+	value T
+}
+
+func NewSimpleForm[T global.Basic[Form]](value T) *SimpleForm[T] {
+	return &SimpleForm[T]{value}
+}
+
+func (sf *SimpleForm[T]) ToString() string {
+	return sf.value.ToString()
+}
+
+func (sf *SimpleForm[T]) Equals(other any) bool {
+	if typed, ok := other.(*SimpleForm[T]); ok {
+		return sf.value.Equals(typed.value)
+	}
+	return false
+}
+
+func (sf *SimpleForm[T]) Copy() Form {
+	return sf.value.Copy()
+}
+
 type Constant struct {
-	value int
+	*SimpleForm[Integer]
+}
+
+func NewConstant(value int) *Constant {
+	return &Constant{NewSimpleForm(Integer(value))}
 }
 
 type Variable struct {
-	value string
+	*SimpleForm[String]
 }
 
-func (c *Constant) ToString() string {
-	return strconv.Itoa(c.value)
+func NewVariable(value string) *Variable {
+	return &Variable{NewSimpleForm(String(value))}
 }
 
-func (c *Constant) Equals(other any) bool {
-	if typed, ok := other.(*Constant); ok {
-		return c.value == typed.value
+type PairForm struct {
+	first  Form
+	second Form
+}
+
+func NewPairForm(first Form, second Form) *PairForm {
+	return &PairForm{first, second}
+}
+
+func (pf *PairForm) ToString() string {
+	return pf.first.ToString() + " " + pf.second.ToString()
+}
+
+func (pf *PairForm) Equals(other any) bool {
+	if typed, ok := other.(*PairForm); ok {
+		return pf.first.Equals(typed.first) && pf.second.Equals(typed.second)
 	}
 	return false
 }
 
-func (c *Constant) Copy() Form {
-	return &Constant{c.value}
+func (pf *PairForm) Copy() Form {
+	return NewPairForm(pf.first.Copy(), pf.second.Copy())
 }
 
-func (v *Variable) ToString() string {
-	return v.value
+type Less struct {
+	*PairForm
 }
 
-func (v *Variable) Equals(other any) bool {
-	if typed, ok := other.(*Variable); ok {
-		return v.value == typed.value
-	}
-	return false
+func NewLess(first Form, second Form) *Less {
+	return &Less{NewPairForm(first, second)}
 }
 
-func (v *Variable) Copy() Form {
-	return &Variable{v.value}
+func (l *Less) ToString() string {
+	return "(" + l.first.ToString() + " < " + l.second.ToString() + ")"
+}
+
+type LessEq struct {
+	*PairForm
+}
+
+func NewLessEq(first Form, second Form) *LessEq {
+	return &LessEq{NewPairForm(first, second)}
+}
+
+func (le *LessEq) ToString() string {
+	return "(" + le.first.ToString() + " <= " + le.second.ToString() + ")"
+}
+
+type Great struct {
+	*PairForm
+}
+
+func NewGreat(first Form, second Form) *Great {
+	return &Great{NewPairForm(first, second)}
+}
+
+func (g *Great) ToString() string {
+	return "(" + g.first.ToString() + " > " + g.second.ToString() + ")"
+}
+
+type GreatEq struct {
+	*PairForm
+}
+
+func NewGreatEq(first Form, second Form) *GreatEq {
+	return &GreatEq{NewPairForm(first, second)}
+}
+
+func (ge *GreatEq) ToString() string {
+	return "(" + ge.first.ToString() + " >= " + ge.second.ToString() + ")"
+}
+
+type Sum struct {
+	*PairForm
+}
+
+func NewSum(first Form, second Form) *Sum {
+	return &Sum{NewPairForm(first, second)}
+}
+
+func (ge *Sum) ToString() string {
+	return "(" + ge.first.ToString() + " + " + ge.second.ToString() + ")"
+}
+
+type Difference struct {
+	*PairForm
+}
+
+func NewDifference(first Form, second Form) *Difference {
+	return &Difference{NewPairForm(first, second)}
+}
+
+func (ge *Difference) ToString() string {
+	return "(" + ge.first.ToString() + " - " + ge.second.ToString() + ")"
+}
+
+type Product struct {
+	*PairForm
+}
+
+func NewProduct(first Form, second Form) *Product {
+	return &Product{NewPairForm(first, second)}
+}
+
+func (ge *Product) ToString() string {
+	return "(" + ge.first.ToString() + " * " + ge.second.ToString() + ")"
 }
